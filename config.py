d75633ae608f4c64c800bd3a88db91410881d822
PB65-Aujla <140618546+PB65-Aujla@users.noreply.github.com>
2024-01-28 19:33:23 +0400

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6608713615"
+OWNER_ID = "6968463708"
 SUPPORT_GRP = "The_ChatGroup"
 UPDATE_CHNL = "The_Support_World"
 OWNER_USERNAME = "PB65_Aujla"
